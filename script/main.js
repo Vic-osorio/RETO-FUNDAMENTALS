6b144885af0e5901cd416c6397a7d0dc66d53b8b
The Victor Code <osoriocrea2021@gmail.com>
2024-09-09 06:36:36 -0500

    reto01

---
+++ script/main.js
@@ -0,0 +1,11 @@
+// Mostrar Banner
+document.addEventListener("DOMContentLoaded", function () {
+    const bannerContainer = document.getElementById("banner");
+    const bannerContent = `
+        <img src="/images/banner.png" alt="Banner Image">
+    `;
+
+    bannerContainer.innerHTML = bannerContent;
+
+})
+
